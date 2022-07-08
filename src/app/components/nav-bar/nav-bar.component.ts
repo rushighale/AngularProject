b6cdfeb4e39b6ca6ef6c0707fd4b12c12c2fa33d
Rushikesh <rushighale007@gmail.com>
2022-07-08 22:37:34 +0530

    change

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+
+  public loggedIn=false;
+  
+
   constructor() { }
 
   ngOnInit(): void {
